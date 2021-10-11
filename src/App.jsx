5a8ce1580ccbad3ad9df921f467e4d8e50de1c08
YUHEI <76258455+yuehi@users.noreply.github.com>
2021-10-11 22:59:44 +0900

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,14 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるtodoは５個までだよ〜。消化しろ〜。
+        </p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
